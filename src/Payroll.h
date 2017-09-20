a1f48f88b10a1e809cb28d5ed00b03628a81f9f2
JoeDMc <31940655+JoeDMc@users.noreply.github.com>
2017-09-20 18:31:12 -0500

    Added my functions from Payroll.h

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,14 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
+float getGrossSalaryPay(float salary);
+float getFIT(float pay);
+float getFICASSN(float pay);
+float getFICAMED(float pay);
+float getNetSPay(float pay);
+float getGrossHourlyOTPay(int hoursWorked, float hourlyPay, float overtimePay, int overtimeHours);
+float getOTPay(int overtimeHours, float hourlyPay);
+int getOTHours(int hoursWorked);
 
 
 #endif
